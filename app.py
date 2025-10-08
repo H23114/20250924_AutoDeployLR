1643bcb507b8ea70597c8cecff5c08009dd9123c
H23114 <89915nicole@gmail.com>
2025-10-08 22:30:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
+import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import streamlit as st
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
